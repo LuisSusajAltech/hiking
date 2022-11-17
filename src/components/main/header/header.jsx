8ae6177daf1e69ff8d99c3780282e71ac6a67806
Luis Susaj <luis@altechsolutions.al>
2022-11-17 20:54:55 +0100

    header

--- src/components/main/header/header.jsx
+++ src/components/main/header/header.jsx
@@ -1,27 +1,35 @@
 import "./header.css"
 import {
     BrowserRouter as Router,
-    Switch,
-    Route,
+    // Switch,
+    // Route,
     Link
   } from "react-router-dom";
+  import images from "../../../assets/images/images"
 function Header(){
     return(
         <div className="header">
+            <img src={images.logo}></img>
             <Router>
                 <div className="nav">
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
+                    <ul className="nav-cont">
+                        <li className="nav-cont-child">
+                            <Link to="/" className="nav-cont-child-link">Home</Link>
                         </li>
-                        <li>
-                            <Link to="/"></Link>
+                        <li className="nav-cont-child">
+                            <Link to="/products" className="nav-cont-child-link">Products</Link>
                         </li>
-                        <li>
-                            <Link to="/">Home</Link>
+                        <li className="nav-cont-child">
+                            <Link to="/hotels" className="nav-cont-child-link">Hotels</Link>
                         </li>
-                        <li>
-                            <Link to="/">Home</Link>
+                        <li className="nav-cont-child">
+                            <Link to="/places" className="nav-cont-child-link">Places</Link>
+                        </li>
+                        <li className="nav-cont-child">
+                            <Link to="/aboutus" className="nav-cont-child-link">About Us</Link>
+                        </li>
+                        <li className="nav-cont-child">
+                            <Link to="/contact" className="nav-cont-child-link">Contact</Link>
                         </li>
                     </ul>
                 </div>
